ee60f024bdbb524cec047afd19350a7ed046b7dc
Le Bon Bauma <baumabulondo@gmail.com>
2021-01-16 20:33:00 +0300

    Creating UserJourney functions.py

---
+++ functions.py
@@ -0,0 +1,19 @@
+# Function to create the chatbot
+# We have the read_only to false so the chatbot learns from the user input as
+
+
+
+# Function to train the bot with a variety of topics
+# the language we have chosen is english
+# we can train the bot for other languages as well
+
+
+
+
+# function to train the bot with custom data
+# it uses listTrainer to train data from Lists
+
+
+
+# Function to start and take responses form the chatbot
+# the chatbot stays runnning unless a world is typed the bye_list
